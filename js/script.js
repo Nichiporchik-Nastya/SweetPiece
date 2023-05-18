4669ac62278d08690e3c6ddb8f9dbf5f94b3449b
Nichiporchik-Nastya <87989987+Nichiporchik-Nastya@users.noreply.github.com>
2023-05-18 23:11:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,42 +1,9 @@
-const productInfos = [
-    {
-        title: "Тарт «Экзотика»",
-        info: "Песочный тарт, крем из маракуйи, желе из экзотических фруктов, воздушное безе, 550г.",
-        price: 21.8,
-        sale: 15
-    },
-    {
-        title: "Тарт «Экзотик2а»",
-        info: "Песочный тарт, крем из маракуйи, желе из экзотических фруктов, воздушное безе, 550г.",
-        price: 25.8,
-        sale: 15
-    },
-    {
-        title: "Тарт «Экзотика»",
-        info: "Песочный тарт, крем из маракуйи, желе из экзотических фруктов, воздушное безе, 550г.",
-        price: 21.8,
-        sale: 15
-    }];
-
 ScrollOut({
     targets: ".animated-item",
     // threshold:0.5,
     // once: false
 });
 
-// let prevScrollpos = window.pageYOffset;
-// window.onscroll = () => {
-
-//     let currentScrollPos = window.pageYOffset;
-//     if (prevScrollpos > currentScrollPos) {
-//         document.querySelector(".header__top").style.top = "-100%";
-//         // console.log('вниз');
-//     } else {
-//         document.querySelector(".header__top").style.top = "100%";
-//         // console.log('вверх');
-//     }
-//     prevScrollpos = currentScrollPos;
-// };
 
 
 let burgerBtn = document.querySelector('.burger-btn'),
@@ -46,7 +13,6 @@ burgerBtn.onclick = () => {
 };
 
 //кнопка бургер-меню
-
 let player = document.querySelector(".reverseEffect");
 let clicked = false;
 player.addEventListener("click", () => {
@@ -63,11 +29,15 @@ player.addEventListener("click", () => {
 });
 
 
-
+//модальное окно
 let modal = document.body.querySelector(".modal_wrapper"),
     modalTitle = document.body.querySelector(".modal_title"),
     modalOldPrice = document.body.querySelector(".modal_block__old-price"),
-    modalNewPrice = document.body.querySelector(".modal_block__price");
+    modalNewPrice = document.body.querySelector(".modal_block__price"),
+    modalInfo = document.body.querySelectorAll(".modal_info"),
+    modalImg = document.body.querySelector(".modal_img"),
+    modalLikes = document.body.querySelector(".modal_likes"),
+    modalCardBottom = document.body.querySelector(".modal_block__bottom");
 
 let modalCloseButton = document.body.querySelector(".modal_close_button");
 if (modalCloseButton) {
@@ -76,41 +46,63 @@ if (modalCloseButton) {
 
 let ids = document.body.querySelectorAll("div[data-product]");
 
-// console.log(ids);
+
 ids.forEach(card => {
     if (card.getBoundingClientRect().left > window.innerWidth) {
         card.classList.remove("animated-item");
     }
     card.addEventListener("click", () => {
-        console.log(card.dataset.product);
-        toggleModal(productInfos[card.dataset.product])
+        toggleModal(card);
     });
 });
 
-function toggleModal(info) {
-    modalTitle.innerHTML = info.title;
-    modalOldPrice.innerHTML = info.price + " руб.";
-    modalNewPrice.innerHTML = info.price - info.price * info.sale * 0.01 + " руб.";
+function toggleModal(card) {
+    modalTitle.innerHTML = card.querySelector(".card__title").innerHTML;
+    modalInfo[0].innerHTML = card.querySelectorAll(".card__info")[0].innerHTML;
+    modalInfo[1].innerHTML = card.querySelectorAll(".card__info")[1].innerHTML;
+    price = parseFloat(card.querySelector(".card__price").innerHTML.slice(0, -4));
+
+    modalOldPrice.innerHTML = price + " руб.";
+    modalNewPrice.innerHTML = +price - (+price * 15 * 0.01) + " руб.";
+    modalImg.src = card.querySelector(".card__img").src;
+    modalLikes.querySelector(".card__rating-number").innerHTML =  card.querySelector(".card__rating-number").innerHTML;
+
+    if(card.querySelector(".likes__like").classList.contains("clicked")){
+        modalLikes.querySelector(".likes__like").classList.add("clicked");
+    }else{
+        modalLikes.querySelector(".likes__like").classList.remove("clicked");
+    }
+
+    if(card.querySelector(".card__cart-btn").classList.contains("card__cart-btn_active")){
+        modalCardBottom.querySelector(".card__cart-btn").classList.add("card__cart-btn_active");
+        modalCardBottom.querySelector(".card__cart-btn").innerHTML = "В корзине &#10004;";
+    }
+    else{
+        modalCardBottom.querySelector(".card__cart-btn").classList.remove("card__cart-btn_active");
+        modalCardBottom.querySelector(".card__cart-btn").innerHTML = "В корзину";
+    }
+    
     modal.classList.toggle("is-open");
     window.addEventListener("scroll", StopScroll);
-
 };
 
 function StopScroll() {
     window.scroll(0, 0);
 }
 
-
 function closeModal() {
     modal.classList.remove("is-open");
     window.removeEventListener("scroll", StopScroll);
 };
 
+
+//возврат 404 страницы
 let backBtn = document.querySelector(".back-btn");
-if(backBtn){
+if (backBtn) {
     backBtn.onclick = () => {
-        // window.location = history.back();
         history.back();
     }
 }
+
+
 
